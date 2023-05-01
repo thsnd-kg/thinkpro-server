cd4e94d2995da979e5ae0f2523e2958a7931b671
Phạm Tấn khang <phamtankhang2001@gmail.com>
2023-05-01 19:57:01 +0700

    config: cloudinary

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,4 +2,4 @@ import { registerAs } from '@nestjs/config';
 
 export const AppConfig = registerAs('app', () => ({
   port: process.env.PORT || 3000,
-}))
+}));
--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,6 @@ import { MongoConfig } from './mongo.config';
 import { JwtConfig } from './jwt.config';
 import { AppConfig } from './app.config';
 import { BcryptConfig } from './bcrypt.config';
+import { CloudinaryConfig } from './cloudinary.config';
 
-export default [MongoConfig, JwtConfig, AppConfig, BcryptConfig];
+export default [MongoConfig, JwtConfig, AppConfig, BcryptConfig, CloudinaryConfig];
