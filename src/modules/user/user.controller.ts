e629206eadccfad847045953851b2de6ec9af56b
Phạm Tấn khang <phamtankhang2001@gmail.com>
2023-05-03 14:35:30 +0700

    refactor: mongoose connection

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -11,11 +11,6 @@ export class UserController {
   constructor(private readonly userService: UserService) {
   }
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.create(createUserDto);
-  }
-
   @Get()
   async findAll(): Promise<User[]> {
     return await this.userService.findAll();
