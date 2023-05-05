9d1ac511371930493f270c97edc19553bed27c95
Phạm Tấn khang <phamtankhang2001@gmail.com>
2023-05-05 22:16:39 +0700

    chore: split file

---
+++ src/modules/cloudinary/cloudinary.module.ts
@@ -0,0 +1,91 @@
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import { CloudinaryService } from './cloudinary.service';
+import { v2 as cloudinary } from 'cloudinary';
+import {
+  CLOUDINARY_MODULE_OPTIONS,
+  CloudinaryModuleAsyncOptions,
+  CloudinaryModuleOptions,
+  CloudinaryModuleOptionsFactory
+} from './cloudinary-options.interface';
+import { CLOUDINARY } from './cloudinary.constant';
+
+@Module({})
+export class CloudinaryModule {
+  static register(options: CloudinaryModuleOptions): DynamicModule {
+    const providers = this.createProviders(options);
+    return {
+      providers,
+      module: CloudinaryModule,
+      exports: providers,
+    };
+  }
+
+  static registerAsync(options: CloudinaryModuleAsyncOptions): DynamicModule {
+    return {
+      module: CloudinaryModule,
+      imports: options.imports || [],
+      providers: [
+        ...this.createAsyncProviders(options),
+        {
+          provide: CLOUDINARY,
+          useFactory: (options: CloudinaryModuleOptions) => {
+            cloudinary.config(options);
+            // Configure Cloudinary here
+            return cloudinary;
+          },
+          inject: [CLOUDINARY_MODULE_OPTIONS],
+        },
+        CloudinaryService,
+      ],
+      exports: [CLOUDINARY_MODULE_OPTIONS, CLOUDINARY, CloudinaryService],
+    };
+  }
+
+  private static createAsyncProviders(
+    options: CloudinaryModuleAsyncOptions,
+  ): Provider[] {
+    if (options.useClass) {
+      return [
+        this.createAsyncOptionsProvider(options),
+        {
+          provide: CLOUDINARY_MODULE_OPTIONS,
+          useClass: options.useClass,
+        },
+      ];
+    }
+
+    return [this.createAsyncOptionsProvider(options)];
+  }
+
+  private static createAsyncOptionsProvider(
+    options: CloudinaryModuleAsyncOptions,
+  ): Provider {
+    if (options.useFactory) {
+      return {
+        provide: CLOUDINARY_MODULE_OPTIONS,
+        useFactory: options.useFactory,
+        inject: options.inject || [],
+      };
+    }
+    // useClass case
+    return {
+      provide: CLOUDINARY_MODULE_OPTIONS,
+      async useFactory(
+        optionsFactory: CloudinaryModuleOptionsFactory,
+      ): Promise<CloudinaryModuleOptions> {
+        return optionsFactory.createCloudinaryOptions();
+      },
+      inject: [options.useClass],
+    };
+  }
+
+  private static createProviders(options: CloudinaryModuleOptions) {
+    return [
+      {
+        provide: CLOUDINARY,
+        useValue: options,
+      },
+      CloudinaryService,
+    ];
+  }
+}
---
+++ src/modules/cloudinary/cloudinary.service.ts
@@ -0,0 +1,46 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { v2 as Cloudinary, UploadApiResponse } from 'cloudinary';
+import toStream from 'buffer-to-stream';
+import { CLOUDINARY } from './cloudinary.constant';
+
+type Cloudinary = typeof Cloudinary;
+
+@Injectable()
+export class CloudinaryService {
+  constructor(
+    @Inject(CLOUDINARY)
+    private readonly cloudinary: Cloudinary) {
+  }
+
+  async upload(file: Express.Multer.File): Promise<UploadApiResponse> {
+    return new Promise((resolve, reject) => {
+      const stream = this.cloudinary.uploader.upload_stream((error, result) => {
+        if (error) return reject(error);
+        resolve(result);
+      });
+      file.stream.pipe(stream);
+    });
+  }
+
+  async streamUpload(file: Express.Multer.File): Promise<UploadApiResponse> {
+    return new Promise((resolve, reject) => {
+      const stream = this.cloudinary.uploader.upload_stream((error, result) => {
+        if (error) {
+          console.log('error', error);
+          return reject(error);
+        }
+        resolve(result);
+      });
+
+      toStream(file.buffer).pipe(stream);
+    });
+  }
+
+  // async delete(public_id: string): Promise<void> {
+  //   await cloudinary.uploader.destroy(public_id);
+  // }
+  //
+  // async getUrl(public_id: string): Promise<string> {
+  //   return cloudinary.url(public_id);
+  // }
+}
